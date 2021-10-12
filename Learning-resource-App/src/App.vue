b80e0d156cb0cf75996402e1cb55445d08c0a572
Ahmad Wali Furmoli <awfurmoli@gmail.com>
2021-10-12 13:39:01 -0700

    Learning Resource App

---
+++ Learning-resource-App/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <the-header :title="'Learning Resources'"></the-header>
+    <stored-resources :resources="storedResources"></stored-resources>
+  </div>
+</template>
+
+
+
+<script>
+import TheHeader from './components/layouts/TheHeader.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
+
+export default {
+  components: { StoredResources, TheHeader },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentaion',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn Google!',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
