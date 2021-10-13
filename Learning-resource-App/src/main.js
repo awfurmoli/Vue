ca4a5a1d6c42ea7b0036a321a42394b05714110b
Ahmad Wali Furmoli <awfurmoli@gmail.com>
2021-10-13 16:52:17 -0700

    Reset The Learning Resource App

--- Learning-resource-App/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue'
-import Basecard from './components/UI/BaseCard.vue'
-import BaseButton from './components/UI/BaseButton.vue'
-const app = createApp(App);
-app.component('base-card', Basecard);
-app.component('base-button', BaseButton);
-app.mount('#app');
